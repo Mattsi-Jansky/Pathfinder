077fd23d723c0e66113b354c495b2866fcff8e1d
Mattsi <mattsi.jansky@codurance.com>
2020-05-02 11:37:38 +0100

    Modify heuristic to improve tests

--- Jansk.Pathfinding.Tests/BasePathfindingTest.cs
+++ Jansk.Pathfinding.Tests/BasePathfindingTest.cs
@@ -6,6 +6,6 @@ namespace Jansk.Pathfinding.Tests
     public class BasePathfindingTest
     {
         protected readonly Func<Tile, Tile, float> _heuristic = (from, to) =>
-            Math.Abs(from.x - to.x) + Math.Abs(from.y - to.y) + Math.Abs(from.z - to.z);
+            (Math.Abs(from.x - to.x) + Math.Abs(from.y - to.y) + Math.Abs(from.z - to.z)) * 1.001f;
     }
 }
--- Jansk.Pathfinding.Tests/Tests/HeuristicTests.cs
+++ Jansk.Pathfinding.Tests/Tests/HeuristicTests.cs
@@ -13,7 +13,7 @@ namespace Jansk.Pathfinding.Tests.Tests
         {
             var map2D = new Map2D(14,14);
             var goalPosition = map2D.Tiles[11, 12];
-            var pathFinder = new PathFinder<Tile>(delegate {return 0;}, 14 * 14, map2D.IndexMap(), map2D.NeighboursManhattan(), map2D.Debug(goalPosition));
+            var pathFinder = new PathFinder<Tile>(delegate {return 0;}, 14 * 14, map2D.IndexMap(), map2D.NeighboursManhattan());
 
             var graph = pathFinder.BuildGraph(map2D.Tiles[0, 2], goalPosition);
 
@@ -25,12 +25,12 @@ namespace Jansk.Pathfinding.Tests.Tests
         {
             var map2D = new Map2D(14,14);
             var goalPosition = map2D.Tiles[11, 12];
-            var pathFinder = new PathFinder<Tile>(_heuristic, 14 * 14, map2D.IndexMap(), map2D.NeighboursManhattan(), map2D.Debug(goalPosition));
+            var pathFinder = new PathFinder<Tile>(_heuristic, 14 * 14, map2D.IndexMap(), map2D.NeighboursManhattan());
 
             var graph = pathFinder.BuildGraph(map2D.Tiles[0, 2], goalPosition);
             var test = graph.Where(x => x != null).Select(x => x.Heuristic).ToList();
 
-            Assert.AreEqual(155, graph.Count(x => x != null));
+            Assert.AreEqual(63, graph.Count(x => x != null));
         }
     }
 }
